86c54e6b38796cc61277d596f4fe6b0fcd757c90
Eugene <yolhovsky@gmail.com>
2016-12-26 16:42:52 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+const int LUCKYNUM=9;
+void introduction ();
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+
+    return 0;
+
+    void introduction(){
+    cout<<"test";
+    cout<<"test";
+    return 0;
+    }
+}
